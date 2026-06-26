4a73667eae496f5c623aeee44fb09700d03e0df0
benyou10 <ybenzian016@gmail.com>
2026-06-26 12:39:33 +0100

    last fix

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,19 +1,21 @@
-import { PrismaClient } from '../generated/prisma/client';
-import { PrismaBetterSqlite3 } from '@prisma/adapter-better-sqlite3';
+import 'dotenv/config'
+// CHANGE THIS LINE to point to your custom output directory:
+import { PrismaClient } from '../generated/prisma' 
+import { PrismaNeon } from '@prisma/adapter-neon'
 
-const databaseUrl = process.env.DATABASE_URL ?? 'file:./dev.db';
+const globalForPrisma = globalThis as unknown as { prisma: PrismaClient }
 
-declare global {
-  // eslint-disable-next-line no-var
-  var prisma: PrismaClient | undefined;
+let prismaInstance: PrismaClient
+
+if (process.env.NODE_ENV === 'production') {
+  const adapter = new PrismaNeon({ connectionString: process.env.DATABASE_URL! })
+  prismaInstance = new PrismaClient({ adapter })
+} else {
+  if (!globalForPrisma.prisma) {
+    const adapter = new PrismaNeon({ connectionString: process.env.DATABASE_URL! })
+    globalForPrisma.prisma = new PrismaClient({ adapter })
+  }
+  prismaInstance = globalForPrisma.prisma
 }
 
-export const prisma = globalThis.prisma ?? new PrismaClient({
-  adapter: new PrismaBetterSqlite3({ url: databaseUrl }),
-});
-
-if (process.env.NODE_ENV !== 'production') {
-  globalThis.prisma = prisma;
-}
-
-export default prisma;
+export const prisma = prismaInstance
